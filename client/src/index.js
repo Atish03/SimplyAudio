60377937da073b3eeb1a9e0d007c79e861b73f97
Atish03 <atishpersonal003@gmail.com>
2023-07-14 10:46:39 +0530

    added desktop only view

--- client/src/index.js
+++ client/src/index.js
@@ -6,17 +6,23 @@ import Tool from './components/Tool';
 import LoginForm from './components/Login';
 import RegisterForm from './components/Register';
 import Home from './components/Home';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/player/" element={ <Tool /> }/>
-      <Route path='/login/' element={ <LoginForm /> }/>
-      <Route path='/register' element={ <RegisterForm /> }/>
-      <Route path='/' element={ <Home /> }/>
-    </Routes>
-  </BrowserRouter>
+  <><BrowserView>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/player/" element={ <Tool /> }/>
+        <Route path='/login/' element={ <LoginForm /> }/>
+        <Route path='/register' element={ <RegisterForm /> }/>
+        <Route path='/' element={ <Home /> }/>
+      </Routes>
+    </BrowserRouter>
+  </BrowserView>
+  <MobileView>
+    Sorry, this is a desktop only site
+  </MobileView></>
 );
 
 reportWebVitals();
